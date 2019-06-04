8c55cb96f7c25c9b90aeff120c92cea5e99704e9
Jason Shockness <jshock12@gmail.com>
2019-06-04 19:09:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
  var a = 5
  var b = 10
  
-function add( a, b)
-return
+function add (a, b)
+return (a + b)
+
 function subtract (b, a)
+return (b - a)
+
 function multiply (a, b )
+return (a * b)
+
 function divide (b, a)
- 
+ return (b / a)
